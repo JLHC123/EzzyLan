dbc09e21e56776d16c70dcf2b5d4f9a5cc0d50a4
JLHC123 <jlhc3751@gmail.com>
2026-09-11 13:10:19 -0700

    the most basic version of reading from the file and producing "tokens"

--- ezzylan_interpreter_n.py
+++ ezzylan_interpreter_n.py
@@ -1,7 +1,6 @@
 file = open("test_2.ezzylan", "r")
 ezzylan = file.read()
 file.close()
-list_of_tokens = []
 tokens = []
 token = ""
 is_string = False
@@ -11,6 +10,28 @@ for char in ezzylan:
         if token != "":
             tokens.append(token)
             token = ""
+    elif char == "\"":
+        if is_string == False:
+            is_string = True
+            tokens.append(char)
+        else:
+            is_string = False
+            tokens.append(token)
+            tokens.append(char)
+            token = ""
+    elif char == ".":
+        if is_string:
+            token += char
+        else:
+            if token != "":
+                tokens.append(token)
+                token = ""
+            tokens.append(char)
+    elif char == "\n":
+        if token != "":
+            tokens.append(token)
+            token = ""
+        tokens.append(char)
     else:
         token += char
 tokens.append(token)
