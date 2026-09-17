feaa4156e8c068146ad213eb109bd7fe156355ce
JLHC123 <jlhc3751@gmail.com>
2026-09-16 20:15:38 -0700

    basic version to test out. If two or more spaces in between Print and 5., we properly address it.

--- print_5.py
+++ print_5.py
@@ -5,9 +5,22 @@ file.close()
 i = 0
 tokens = []
 token = ""
+space_before = False
+
 
 while i < len(ezzylan):
-    print(ezzylan[i])  
-    i += 1  
+    character = ezzylan[i]
+    if character == " ":
+        # if there are two or more spaces back to back, skip this part
+        if space_before == False:
+            tokens.append(token)
+            space_before = True
+        tokens.append(" ")
+        token = ""
+    else:
+        token += character
+        space_before = False
+    i += 1 
+tokens.append(token)
         
-print(token)
+print(tokens)
