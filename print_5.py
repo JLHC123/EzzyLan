d62911b3178ae0ca1f50c3ee5c9d165a56d95702
JLHC123 <jlhc3751@gmail.com>
2026-09-16 21:34:42 -0700

    ok we can read per line, right now just for handling multiple test cases as I want to create multiple exception cases or error statements to handle each erroneous input.

--- print_5.py
+++ print_5.py
@@ -1,26 +1,32 @@
-file = open("print_5.ezzylan", "r")
-ezzylan = file.read()
-file.close()
+def tokenizer(ezzylan):
+    i = 0
+    tokens = []
+    token = ""
+    space_before = False
+    while i < len(ezzylan):
+        character = ezzylan[i]
+        if character == " ":
+            # if there are two or more spaces back to back, skip this part
+            if space_before == False:
+                tokens.append(token)
+                space_before = True
+            tokens.append(" ")
+            token = ""
+        else:
+            token += character
+            space_before = False
+        i += 1 
+    tokens.append(token)        
+    print(tokens)
 
-i = 0
-tokens = []
-token = ""
-space_before = False
+def main():
+    file = open("print_5.ezzylan", "r")
+    ezzylan_test = file.read()
+    file.close()
+    
+    lines = ezzylan_test.split("\n")
+    for line in lines:
+        tokenizer(line)
 
-
-while i < len(ezzylan):
-    character = ezzylan[i]
-    if character == " ":
-        # if there are two or more spaces back to back, skip this part
-        if space_before == False:
-            tokens.append(token)
-            space_before = True
-        tokens.append(" ")
-        token = ""
-    else:
-        token += character
-        space_before = False
-    i += 1 
-tokens.append(token)
-        
-print(tokens)
+if __name__ == "__main__":
+    main()
