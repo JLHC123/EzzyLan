0015ec370f4c55db590378209b6308bdb75846d7
JLHC123 <jlhc3751@gmail.com>
2026-09-22 21:28:54 -0700

    We refactor the multiple if statements with key_words, so that we are able to convert print into "PRINT", any number into "NUMBER", "." into "END", etc.
    Well, for any number into "NUMBER" we are using token.isdigit().

--- print_5.py
+++ print_5.py
@@ -32,25 +32,18 @@ def tokenizer(ezzylan):
     # print(tokens)
     return(tokens)
 
-def convert(tokens):
+def convert(tokens, key_words):
     instructions = []
     if not tokens:
         return instructions
     for token in tokens:
-        if token == "Print":
-            instruction = "PRINT"
-        elif token == " ":
-            instruction = "SPACE"
-        elif token == ".":
-            instruction = "END"        
+        if token in key_words:
+            instructions.append(key_words[token])
         elif token.isdigit():
-            instruction = "NUMBER"
-        else:
-            instruction = token
-        instructions.append(instruction)
+            instructions.append("NUMBER")
     return instructions
 
-# will refine this
+# will refine this (when I have more "valid statements")
 def execute(instructions, tokens):
     if instructions == ['PRINT', 'SPACE', 'NUMBER', 'END']:
         for token in tokens:
@@ -59,22 +52,25 @@ def execute(instructions, tokens):
                 print(number)
                 return
     else:
-        print("Not a valid line")
-        
-        
-        
-                
+        print("Not a valid line")             
     
 def main():
     file = open("print_5.ezzylan", "r")
     ezzylan_test = file.read()
     file.close()
     
+    key_words = {
+        "Print": "PRINT",
+        ".": "END",
+        " ": "SPACE",
+    }
+    
     lines = ezzylan_test.split("\n")
     for line in lines:
         tokens = tokenizer(line)
         # print(tokens)
-        instructions = convert(tokens)
+        instructions = convert(tokens, key_words)
+        # print(instructions)
         execute(instructions, tokens)
 
 
